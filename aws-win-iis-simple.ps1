b51866950ed71965c9ffbc1151be731bd56a1226
Thomas Drilling <37440018+tdrilling@users.noreply.github.com>
2023-01-21 21:08:58 +0100

    Update aws-win-iis-simple.ps1

--- aws-win-iis-simple.ps1
+++ aws-win-iis-simple.ps1
@@ -1,3 +1,6 @@
+<powershell>
 powershell.exe Install-WindowsFeature -name Web-Server -IncludeManagementTools
 powershell.exe Remove-Item -Path 'C:\inetpub\wwwroot\iisstart.htm'
 powershell.exe Add-Content -Path 'C:\inetpub\wwwroot\iisstart.htm' -Value "$env:computername"
+</powershell>
+<persist>true</persist>
